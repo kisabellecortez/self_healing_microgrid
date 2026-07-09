c0e5dcd601c1c2a15baae4666d767c176de12226
kisabellecortez <kisabellecortez@gmail.com>
2026-07-09 16:36:49 -0400

    Developed scheduler to run retraining script automatically.

--- islanding_api/main.py
+++ islanding_api/main.py
@@ -1,6 +1,42 @@
 from fastapi import FastAPI
+from apscheduler.schedulers.background import BackgroundScheduler
+import subprocess
+import sys
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
+
+def run_training_script():
+    try:
+        subprocess.run(
+            [
+                sys.executable,
+                "retrain_isolation_forests.py"
+            ]
+        )
+
+    except subprocess.CalledProcessError as e:
+        print("Training failed: ", e.stderr)
+
+@app.on_event("startup")
+def startup_event():
+    scheduler.start()
+
+    scheduler.add_job(
+        run_training_script,
+        "cron",
+        hour=3,
+        minute=0,
+        id="daily_retraining",
+        replace_existing=True
+    )
+
+    scheduler.start()
+
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
 
 @app.get("/")
 def home():
