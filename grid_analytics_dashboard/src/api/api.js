aa665566ee1961741f7ffb5965b5ca00049ae615
kisabellecortez <kisabellecortez@gmail.com>
2026-07-22 00:21:49 -0400

    Added API functions for analytics dashboard home page and fetching weather features using OpenWeather.

---
+++ grid_analytics_dashboard/src/api/api.js
@@ -0,0 +1,61 @@
+const API_URL = ""
+
+export async function getCurrentStatus(){
+    const response = await fetch(
+        `${API_URL}/dashboard_functions/status`
+    );
+
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch status.")
+    // }
+
+    return await response.json()
+}
+
+export async function getCurrentPower(){
+    const response = await fetch(
+        `${API_URL}/dashboard_functions/power`
+    );
+
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch power.")
+    // }
+    
+    return await response.json()
+}
+
+export async function getCurrentEnergy(){
+    const response = await fetch(
+        `${API_URL}/dashboard_functions/energy`
+    );
+
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch energy.")
+    // }
+
+    return await response.json()
+}
+
+export async function getWeather(){
+    const response = await fetch(
+        `${API_URL}/api/weather`
+    );
+
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch weather features.")
+    // }
+
+    return await response.json()
+}
+
+export async function getLoadsData(){
+    const response = await fetch(
+        `${API_URL}/dashboard_functions/loads_data`
+    );
+
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch load data.")
+    // }
+
+    return await response.json()
+}
